6bf892f558fa8fee00d463382dd33e3f32203cb6
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2011-11-30 05:55:44 +0000

    updated footer

--- search/incs/footer.php
+++ search/incs/footer.php
@@ -1,5 +1,5 @@
 <div class="footer">
 <font size=2>The VA/PT Risk Management, Vulnerability Assessment and Penetration Testing Portal.</font>
-<p><font size=2><i><a href="mailto:enforce570@gmail.com"> Keith A. Pachulski</a> - Information Security Consulting &#169; 2010, 2011</i></font> 
+<p><font size=2><i><a href="http://www.protectors.cc/" target="_blank"> Keith A. Pachulski</a> - Information Security Consulting &#169; 2010, 2011</i></font> 
 </div>
 
