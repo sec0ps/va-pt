e50d55105c06deec228725501a47572c54fc17ac
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2012-01-22 18:49:46 +0000

    updates - compacting and removing sloppy stuff

--- portal/application/incs/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="footer">
-<font size=2>The VA/PT Vulnerability Assessment and Penetration Testing Database Interface. Comments and suggestions? Contact me <a href="mailto:enforce570@gmail.com"> Keith A. Pachulski</a></font>
-</div>
-
