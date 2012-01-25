769a300e03d2465d37de8e25ec7525d13bebc207
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2012-01-25 20:02:04 +0000

    update and cleanup

--- trunk/search/index.php
+++ trunk/search/index.php
@@ -1,15 +1,12 @@
 <?php
-	if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1')
-	exit( print "Connections to this portal are not permitted from remote systems" );
+    if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1')
+    exit( print "Connections to this portal are not permitted from remote systems" );
 ?>
 <?php
-// destroy previous session
 session_start();
 session_regenerate_id();
 
 require 'incs/define.php';
-//require 'incs/authorize.php';
-//require 'incs/forcessl.inc';
 ?>
 
 <html>
@@ -34,9 +31,23 @@ require 'incs/define.php';
  </form>
 </div>
 
-<?php // require 'incs/menu.inc' ?>
+<div class="application">
+<font size=2>
+<p><b><u>The Vulnerability Assessment and Penetration Testing Toolkit - Vulnerability and Exploit Search Portal</b></u></p>
 
-<?php require 'incs/footer.php' ?>
+<p>The Vulnerability and Exploit Search Portal will be migrated into the overall project management framework once it is completed.<br>
+It will be distributed as part of the overall "The Risk Assessment, Vulnerability Assessment and Penetration Testing Management Portal"</p>
 
+<p>The kit is being provided during development under <a href="http://www.gnu.org/licenses/old-licenses/gpl-2.0.html" target="_blank">GNU GPLv2</a></p>
+
+<p>If you have any comments, questions, suggestions or to report bugs, please use the <a href="http://code.google.com/p/va-pt/issues/list" target="_blank">Issue Tracker</a> on Google Code.</p> 
+
+<p>Thanks,<br>
+Keith Pachulski<br> 
+Twitter: <a href="https://twitter.com/#!/sec0ps" target="_blank">@sec0ps</a><br>
+Email: <a href="mailto:enforce570@gmail.com">enforce570@gmail.com</a>
+
+</font>
+</div>
 </body>
 </html>
