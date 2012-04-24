a3528cd7f6640cfba6d2778575b4a98e4b7745e2
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2012-04-24 17:23:40 +0000

    added firebird database tools

--- trunk/scripts/deps.sh
+++ trunk/scripts/deps.sh
@@ -24,7 +24,7 @@ sudo apt-get install -y bkhive xprobe2 safecopy ptunnel ngrep btscanner cabextra
 sudo apt-get install -y galleta pasco vinetto autopsy wine1.3-dev rifiuti2 recover extundelete recoverdm ext3grep python-gtksourceview2 zlib1g-dev 
 sudo apt-get install -y libasound2-dev libbz2-dev vlc libvlc-dev gtk2-engines-pixbuf perl-doc ike-scan xchat outguess steghide python-septic snowdrop
 sudo apt-get install -y flex libidn11-dev zlib-bin zlibc ruby-openssl texlive-latex-extra liblzo2-dev python-pysqlite2 sslsniff libnet-netmask-perl
-sudo apt-get install -y inguma libreadline-dev registry-tools
+sudo apt-get install -y inguma libreadline-dev registry-tools flamerobin
 
 #Installing google earth
 #sudo apt-get install googleearth-package
