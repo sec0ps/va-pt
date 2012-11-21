17c86a4a125d7ee4420a9dc22b32df9bcc824fc4
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2012-11-21 03:02:54 +0000

    dradis updates

--- scripts/static.sh
+++ scripts/static.sh
@@ -348,10 +348,10 @@ mv rdp-sec-check-0.8 /pentest/enumeration/rdp-sec-check
 fi
 if [ ! -d /pentest/misc/dradis ] ; then
 echo "Installing Dradis"
-cd /pentest/temp && wget http://sourceforge.net/projects/dradis/files/dradis/v2.8.0/dradis-v2.8.0.tar.bz2/download -O dradis-v2.8.0.tar.bz2
-bunzip2 dradis-v2.8.0.tar.bz2 && tar xvf dradis-v2.8.0.tar
-rm -rf dradis-v2.8.0.tar && mv dradis-2.8/ /pentest/misc/dradis
-cd /pentest/misc/dradis/server && /var/lib/gems/1.8/bin/bundle install
+cd /pentest/temp && wget http://downloads.sourceforge.net/dradis/dradis-v2.9.0.tar.bz2
+bunzip2 dradis-v2.9.0.tar.bz2 && tar xvf dradis-v2.9.0.tar
+rm -rf dradis-v2.9.0.tar && mv dradis-2.9/ /pentest/misc/dradis
+cd /pentest/misc/dradis/server && bundle install
 cd /pentest/misc/dradis && ./reset.sh
 fi
 if [ ! -d /pentest/wireless/hwk ] ; then
