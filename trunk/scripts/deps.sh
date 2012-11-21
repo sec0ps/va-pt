dd6f29d5b9d0fbc0b634a013ec53b8224fb63258
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2012-11-21 03:02:54 +0000

    dradis updates

--- trunk/scripts/deps.sh
+++ trunk/scripts/deps.sh
@@ -120,8 +120,9 @@ sudo gem install bson_ext
 sudo gem install rake
 sudo gem install rails
 sudo gem install RedCloth
-sudo gem install bundle
+sudo gem install bundler
 sudo gem install sqlite3
+sudo gem install sqlite3-ruby
 sudo gem install wxruby
 sudo gem install rake-compiler
 sudo gem install jeweler
