d1cf231b7d66db2817c929f003cbc2e65989b2bd
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2013-05-25 12:27:40 +0000

    added openvas5 installer for ubuntu 12.04, removed older openvas installer scripts

--- trunk/scripts/ovas1204.sh
+++ trunk/scripts/ovas1204.sh
@@ -1,18 +1,20 @@
-#install openvas 4 deps and software repo
+echo "Installing OpenVAS 5 for Ubuntu 12.04LTS"
+echo "Configuring OBS Repository"
 sudo apt-get -y install python-software-properties
-sudo add-apt-repository "deb http://download.opensuse.org/repositories/security:/OpenVAS:/STABLE:/v4/xUbuntu_10.04/ ./"
+sudo add-apt-repository "deb http://download.opensuse.org/repositories/security:/OpenVAS:/UNSTABLE:/v5/xUbuntu_12.04/ ./"
 sudo apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys BED1E87979EAFD54
 sudo apt-get update
-sudo apt-get -y install greenbone-security-assistant gsd openvas-cli openvas-manager openvas-scanner openvas-administrator sqlite3 xsltproc
-#configure openvas 4
+#
+echo "Installing OpenVAS 5 Packages"
+sudo apt-get -y --force-yes install greenbone-security-assistant gsd openvas-cli openvas-manager openvas-scanner openvas-administrator xsltproc
+#
+echo "Configuring the OpenVAS Server"
 test -e /var/lib/openvas/CA/cacert.pem  || sudo openvas-mkcert -q
 sudo openvas-nvt-sync
 test -e /var/lib/openvas/users/om || sudo openvas-mkcert-client -n om -i
 sudo /etc/init.d/openvas-manager stop
 sudo /etc/init.d/openvas-scanner stop
-sudo openvassd
-sudo openvasmd --migrate
-sudo openvasmd --rebuild
+sudo openvassd && sudo openvasmd --rebuild
 sudo killall openvassd
 sleep 15
 sudo /etc/init.d/openvas-scanner start
@@ -20,7 +22,5 @@ sudo /etc/init.d/openvas-manager start
 sudo /etc/init.d/openvas-administrator restart
 sudo /etc/init.d/greenbone-security-assistant restart
 test -e /var/lib/openvas/users/admin || sudo openvasad -c add_user -n admin -r Admin
-#start GSD
-gsd &
 
-#http://download.opensuse.org/repositories/security:/OpenVAS:/STABLE:/v4/xUbuntu_11.10/
+
