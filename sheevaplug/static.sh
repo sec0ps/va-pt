d861f7b4340a3b6af95d38d73058596b55075d33
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2013-10-04 13:50:15 +0000

    sheeva modifications

--- sheevaplug/static.sh
+++
@@ -1,9 +0,0 @@
-ruby -v | grep "1.9.3"
-if [ $? -eq 1 ] ; then
-echo "Installing Ruby 1.9.3"
-cd /pentest/temp && wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p448.tar.gz
-tar xvf ruby-1.9.3-p448.tar.gz && rm -rf ruby-1.9.3-p448.tar.gz
-cd ruby-1.9.3-p448 && ./configure && make
-sudo make install
-fi
-
