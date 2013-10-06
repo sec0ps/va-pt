cc91d19ca8d9634c562ce592c037ee83e01e98e1
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2013-10-06 16:29:16 +0000

    more sheeva stuff

--- sheevaplug/deps.sh
+++ sheevaplug/deps.sh
@@ -1,6 +1,6 @@
-apt-get install -y gcc sudo subversion ntpdate autoconf make libssl-dev libpcap-dev libpcre3-dev
-apt-get install -y git g++ vim libyaml-dev postgresql libpq-dev python-setuptools python-pip python2.7-dev libsqlite3-dev 
-apt-get install -y libxslt1-dev libxml2-dev python-pip iw libncurses-dev libnl-dev pkg-config mtd-utils
+sudo apt-get install -y gcc sudo subversion ntpdate autoconf make libssl-dev libpcap-dev libpcre3-dev
+sudo apt-get install -y git g++ vim libyaml-dev postgresql libpq-dev python-setuptools python-pip python2.7-dev libsqlite3-dev
+sudo apt-get install -y libxslt1-dev libxml2-dev python-pip iw libncurses-dev libnl-dev pkg-config mtd-utils
 
 if [ ! -f /usr/local/bin/cpanm ] ; then
 echo "Installing CPANimus"
