351d4b48c443ab05d63ebba5da5c2841d9f23b1a
enforce570@gmail.com <enforce570@gmail.com@029054bc-00af-3e94-fb8b-f148bdaa926b>
2013-10-30 00:53:31 +0000

    rasp/sheeva modifications

--- rasp-pi/deps.sh
+++ rasp-pi/deps.sh
@@ -13,3 +13,31 @@ cd cpanminus && perl Makefile.PL
 make && sudo make install
 cd /pentest/temp && rm -rf cpanminus/
 fi
+
+if [ ! -f /usr/local/lib/perl/5.14.2/Math/Pari.pm ] ; then
+echo "Installing PERL Libraries"
+cd /pentest/temp && wget http://pari.math.u-bordeaux.fr/pub/pari/unix/OLD/pari-2.1.7.tgz
+tar xvf pari-2.1.7.tgz && rm -rf pari-2.1.7.tgz
+cd pari-2.1.7/ && wget http://search.cpan.org/CPAN/authors/id/I/IL/ILYAZ/modules/Math-Pari-2.01080605.tar.gz
+tar xvf Math-Pari-2.01080605.tar.gz && rm -rf Math-Pari-2.01080605.tar.gz
+cd Math-Pari-2.01080605 && perl Makefile.PL
+sudo make install
+else
+echo "Pari is installed, moving on"
+fi
+
+sudo cpanm Net::SSLeay
+sudo cpanm HTTP::Request::Common
+sudo cpanm LWP::UserAgent
+sudo cpanm WWW::Mechanize
+sudo cpanm Net::Telnet
+sudo cpanm Net::SSH::Perl
+
+ruby -v | grep "1.9.3"
+if [ $? -eq 1 ] ; then
+echo "Installing Ruby 1.9.3"
+cd /pentest/temp && wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz
+tar xvf ruby-1.9.3-p392.tar.gz && rm -rf ruby-1.9.3-p392.tar.gz
+cd ruby-1.9.3-p392 && ./configure && make
+sudo make install
+fi
