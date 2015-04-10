23ce873184568b7c4b9132edbd414d9e9b570721
Keith A. Pachulski <enforce570@gmail.com>
2015-04-10 11:07:10 -0400

    modified watchdog

--- rasp-pi/tools/wvdial-watchdog.sh
+++ rasp-pi/tools/wvdial-watchdog.sh
@@ -1,4 +1,5 @@
 #watchdog script for aws using ppp0 interface
+#created by Jason Frisvold aka @XenoPhage
 sudo /usr/sbin/hping3 -c 2 -p 22 -I ppp0 <hostname> > /tmp/wvdialchecker
 
 RC=`grep "flags=RA" /tmp/wvdialchecker | wc -l`
