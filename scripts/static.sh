9c8e40db97cfad6d4f9885c0fddaecd292df4ae3
Keith Pachulski <enforce570@gmail.com>
2015-08-18 16:39:53 -0400

    corrected install path

--- scripts/static.sh
+++ scripts/static.sh
@@ -404,7 +404,7 @@ if [ ! -d /pentest/enumeration/medusa ] ; then
 echo "Installing Medusa"
 cd /pentest/temp && wget https://github.com/jmk-foofus/medusa/releases/download/2.2_rc2/medusa-2.2_rc2.tar.gz
 tar medusa-2.2_rc2.tar.gz && cd medusa-2.2_rc2/
-mv medusa-2.2_rc2/ /pentest/enumeration/medusa
+mv medusa-2.2_rc2/ /pentest/enumeration/medusa && cd /pentest/enumeration/medusa 
 ./configure && make
 fi
 echo "Static Code installation complete"
