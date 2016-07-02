2615c34ca636b538b412a188fc8db98449ff9098
Keith Pachulski <enforce570@gmail.com>
2016-07-01 22:25:08 -0400

    added crackmapexec

--- scripts/svn.sh
+++ scripts/svn.sh
@@ -354,6 +354,11 @@ echo "Installing Rawr - Rapid Assessment of Web Resources"
 cd /pentest/web/ && git clone https://bitbucket.org/al14s/rawr.git
 cd rawr && sudo ./install.sh
 fi
+if [ ! -d /pentest/exploits/CrackMapExec ] ; then
+echo "Installing CrackMapExec"
+cd /pentest/exploits && git clone https://github.com/byt3bl33d3r/CrackMapExec.git
+cd CrackMapExec && sudo python setup.py install
+fi
 echo "Installing local tools"
 cp /pentest/misc/va-pt/tools/copy-router-config.pl /pentest/cisco/
 cp /pentest/misc/va-pt/tools/merge-router-config.pl /pentest/cisco/
