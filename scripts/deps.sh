2f290d61af57e3cda5deea51023b9cc64ea374e1
keithp <enforce570@gmail.com>
2017-05-05 21:16:20 -0400

    added openconnect

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -23,7 +23,7 @@ sudo apt-get install -y ptunnel iodine udptunnel httptunnel netmask dnstracer dn
 sudo apt-get install -y libbsd-dev unixodbc unixodbc-dev freetds-dev sqsh tdsodbc autofs remmina remmina-plugin-rdp remmina-plugin-vnc
 sudo apt-get install -y squid python-libpcap ntpdate screen samba-common-bin upx whois libreadline-gplv2-dev gcc-mingw-w64-x86-64
 sudo apt-get install -y gcc-mingw-w64-i686 libsqlite3-dev tftp tftpd libfreerdp-dev libssh2-1-dev python-elixir python-pyasn1
-
+sudo apt-get install -y openconnect
 #crunch
 
 ruby -v | grep "2.3.3"
