ecd698d912b363a6a7776b35fa8c4697914cb1c9
Keith Pachulski <enforce570@gmail.com>
2020-12-18 12:54:55 -0500

    removed cewl package, added as git repo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "Installing base packages and locking down the system - denying all inbound
 echo "This may take a few minutes.."
 echo "Installing Base Packages and Dependencies"
 sudo apt install vim subversion landscape-common ufw openssh-server net-tools libpangox-1.0-dev mlocate ntpdate screen whois kate libtool-bin
-sudo apt install -y make gcc ncftp rar p7zip-full curl libpcap-dev libssl-dev hping3 libssh-dev g++ arp-scan cewl wifite ruby-bundler freerdp2-dev
+sudo apt install -y make gcc ncftp rar p7zip-full curl libpcap-dev libssl-dev hping3 libssh-dev g++ arp-scan wifite ruby-bundler freerdp2-dev
 sudo apt install -y libsqlite3-dev nbtscan dsniff apache2 secure-delete autoconf libpq-dev libmysqlclient-dev libsvn-dev libssh-dev libsmbclient-dev
 sudo apt install -y libgcrypt-dev libbson-dev libmongoc-dev python3-pip netsniff-ng httptunnel ptunnel-ng udptunnel pipx python3-venv ruby-dev
 sudo ln -s /usr/bin/python3 /usr/bin/python
