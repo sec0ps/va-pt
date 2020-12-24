49d6e66584dbec82b1aca9b4dc09726989632678
sec0ps <enforce570@gmail.com>
2020-12-23 20:43:48 -0500

    correcting arachni installer

--- static.sh
+++ static.sh
@@ -14,6 +14,7 @@ mv ZAP* zap/
 fi
 if [ ! -d /vapt/web/arachni ] ; then
 cd /vapt/web && wget https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-linux-x86_64.tar.gz
+tar xvf arachni-1.5.1-0.5.12-linux-x86_64.tar.gz
 mv arachni-1.5.1-0.5.12/ arachni/ && rm arachni-1.5.1-0.5.12-linux-x86_64.tar.gz
 fi 
 if [ ! -f /vapt/scanners/copy-router-config.pl ] ; then
