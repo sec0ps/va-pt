0c7dd76e1ec07595df70e2376e4d3f4f9e010700
sec0ps <enforce570@gmail.com>
2021-01-25 13:20:49 -0500

    added metafinder

--- update-tools.sh
+++ update-tools.sh
@@ -30,6 +30,7 @@ cd /vapt/intel/spiderfoot && git pull
 cd /vapt/intel/theHarvester && git pull
 cd /vapt/intel/GRecon && git pull
 cd /vapt/intel/sherlock && git pull
+cd /vapt/intel/MetaFinder && git pull
 
 echo "Updating Password Tools"
 cd /vapt/passwords/CeWL && git pull
