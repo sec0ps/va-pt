76ac19dcc5ddecbf02d8e009dbbd2e213ef02434
sec0ps <enforce570@gmail.com>
2021-12-10 09:07:48 -0500

    updates for installer and wordlists

--- wordlist.sh
+++ wordlist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f /vapt/passwords/weakpass_2 ] ; then
 echo "Downloading the weakpass archive"
-cd /vapt/passwords && curl https://download.weakpass.com/wordlists/1863/weakpass_2.gz > weakpass_2.gz
+cd /vapt/passwords && curl https://download.weakpass.com/wordlists/1948/weakpass_3a.7z > weakpass_3a.gz
 fi
