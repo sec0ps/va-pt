65f93a8b8430bf5ce7e8df3490941eb1833b18b5
sec0ps <enforce570@gmail.com>
2021-12-12 18:59:53 -0500

    added new weakpass 3a dictionary file

--- wordlist.sh
+++ wordlist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ ! -f /vapt/passwords/weakpass_2 ] ; then
+if [ ! -f /vapt/passwords/weakpass_3a ] ; then
 echo "Downloading the weakpass archive"
-cd /vapt/passwords && curl https://download.weakpass.com/wordlists/1948/weakpass_3a.7z > weakpass_3a.gz
+cd /vapt/passwords && wget https://download.weakpass.com/wordlists/1948/weakpass_3a.7z
+7z e weakpass_3a.7z
 fi
