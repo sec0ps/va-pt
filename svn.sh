672b64cb20c8f3df9371bfa069f118cdaceb26bb
Keith Pachulski <enforce570@gmail.com>
2023-06-19 10:14:56 -0500

    added linkedin scraper

--- svn.sh
+++ svn.sh
@@ -161,6 +161,10 @@ if [ ! -d /vapt/intel/sherlock ] ; then
 cd /vapt/intel && git clone https://github.com/sherlock-project/sherlock.git
 cd sherlock && python3 -m pip install -r requirements.txt
 fi
+if [ ! -d /vapt/intel/LinkedInDumper ] ; then
+cd /vapt/intel && git clone https://github.com/l4rm4nd/LinkedInDumper.git
+cd LinkedInDumper && pip install -r requirements.txt 
+fi
 
 #Password tools
 if [ ! -d /vapt/passwords/JohnTheRipper ] ; then
