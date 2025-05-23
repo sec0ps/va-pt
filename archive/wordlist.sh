d1da723d77cbe7d6bd2c1b9880195f619ffdeab3
Keith Pachulski <sec0ps@users.noreply.github.com>
2025-05-23 17:13:16 -0500

    Delete archive directory

--- archive/wordlist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -f /vapt/passwords/weakpass_3a ] ; then
-echo "Downloading the weakpass archive"
-cd /vapt/passwords && wget https://download.weakpass.com/wordlists/1948/weakpass_3a.7z
-7z e weakpass_3a.7z
-fi
