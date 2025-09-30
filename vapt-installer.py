33f2a1d3e03006c6b3c55f0ee8c9192b2b865b0b
Keith Pachulski <sec0ps@users.noreply.github.com>
2025-09-30 12:07:36 -0500

    Update vapt-installer.py

--- vapt-installer.py
+++ vapt-installer.py
@@ -183,7 +183,7 @@ def install_base_dependencies():
     run_command("sudo apt install -y libgcrypt-dev libbson-dev libmongoc-dev python3-pip netsniff-ng httptunnel ptunnel-ng udptunnel pipx python3-venv ruby-dev")
     run_command("sudo apt install -y webhttrack minicom openjdk-21-jre gnome-tweaks macchanger recordmydesktop postgresql golang-1.23-go* hydra-gtk hydra")
     run_command("sudo apt install -y ncftp wine-development libcurl4-openssl-dev smbclient hackrf nfs-common samba gpsd")
-    run_command("sudo apt install -y docker.io docker-compose hcxtools httrack")
+    run_command("sudo apt install -y docker.io docker-compose hcxtools httrack tshark")
     run_command("sudo apt install -y rbenv libffi-dev libyaml-dev libreadline-dev libncurses5-dev libgdbm-dev zlib1g-dev build-essential bison libedit-dev libxml2-utils")
     run_command("sudo usermod -aG docker $USER")
     run_command("sudo snap install powershell --classic")
